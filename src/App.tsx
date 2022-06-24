770b4b1c96f59cf2b3012925c9ca593a69683b54
Gaven Nelson <gnelson@bushelpowered.com>
2022-06-24 09:51:58 -0500

    App outline set

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import logo from "./logo.svg";
+import { useState, useEffect } from "react";
+import { useSearchParams, useParams, useNavigate } from "react-router-dom";
 import "./App.css";
 import {
   Box,
@@ -13,18 +13,45 @@ import {
   Input,
   InputGroup,
   InputLeftElement,
+  SimpleGrid,
 } from "@chakra-ui/react";
+import Loader from "./components/Loading";
+import SearchIcon from "@chakra-ui/icons";
+import pokedexText from "/PokemonText.png";
 
 function App() {
-  let pokemonURL = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png`;
+  const [pokemon, setPokemon] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState([]);
+
+  interface Pokemon {
+    id: number;
+    name: string;
+    types: string;
+  }
+
+  useEffect(() => {
+    fetch("https://intern-pokedex.myriadapps.com/api/v1/pokemon")
+      .then((response) => response.json())
+      .then((res) => console.log(res))
+      .catch((err) => setError(err));
+  }, []);
 
   return (
-    <div className="pageContainer">
-      <div className="pageHeader">
-        <Flex flexDirection="row" justifyContent="space-between" alignItems="center" bg='rgb(0, 128, 128, .4)' paddingTop='1.5%' paddingBottom='1.5%'>
-          <div className="leftButton">
+    <Box className="pageContainer">
+      <Box className="pageHeader">
+        <Flex
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+          bg="rgb(0, 128, 128, .4)"
+          paddingTop="1.5%"
+          paddingBottom="1.5%"
+        >
+          <Box paddingLeft="5">
             <Button
-              size="lg"
+              bg="teal"
+              size={["sm", "md", "lg"]}
               colorScheme="teal"
               variant="solid"
               borderRadius="50%"
@@ -32,44 +59,81 @@ function App() {
             >
               &#10094;
             </Button>
-          </div>
-          <Box fontSize="2em" >
-            Pokedex
           </Box>
-          <div className="searchBar">
-            <InputGroup>
-              <InputLeftElement
-                pointerEvents="none"
-                fontSize="2em"
-                paddingTop='.8%'
-                paddingLeft='.5%'
-                children= "&#128270;"
-              ></InputLeftElement>
-              <Input placeholder="Search" _placeholder={{color: 'white'}} size="lg" width="80%"></Input>
+          <Box>
+            <Image src={pokedexText} maxW={["20", "100", "250"]} />{" "}
+          </Box>
+          {/* <Box>
+            <Image
+              src="https://projectpokemon.org/images/normal-sprite/bulbasaur.gif"
+              maxW={["0", "50", "100"]}
+            />{" "}
+          </Box> */}
+          <Box width="60%">
+            <InputGroup bg="whiteAlpha.100" justifyContent="center">
+              <Input
+                flexGrow="1"
+                placeholder="Search"
+                _placeholder={{ color: "white" }}
+                size={["sm", "md", "lg"]}
+                width="80%"
+                color="white"
+                outline="gray"
+                fontSize="50"
+              ></Input>
             </InputGroup>
-          </div>
+          </Box>
 
-          <div className="rightButton">
+          <Box paddingRight="5">
             <Button
-              size="lg"
+              bg="teal"
+              size={["sm", "md", "lg"]}
               colorScheme="teal"
               variant="solid"
               borderRadius="50%"
-              fontSize="1.3em"
             >
               &#10095;
             </Button>
-          </div>
+          </Box>
         </Flex>
-      </div>
-      <div className="pageBody">
-
-
-      </div>
-    </div>
-
-    
+      </Box>
+      <Box className="pageBody">
+        <SimpleGrid
+          columns={[2, 3, 4, 5]}
+          spacing="10"
+          paddingLeft="5"
+          paddingRight="5"
+          paddingTop="5"
+        >
+          <Box bg="teal" height="150px"></Box>
+          <Box bg="teal" height="150px"></Box>
+          <Box bg="teal" height="150px"></Box>
+          <Box bg="teal" height="150px"></Box>
+          <Box bg="teal" height="150px"></Box>
+          <Box bg="teal" height="150px"></Box>
+          <Box bg="teal" height="150px"></Box>
+          <Box bg="teal" height="150px"></Box>
+          <Box bg="teal" height="150px"></Box>
+          <Box bg="teal" height="150px"></Box>
+        </SimpleGrid>
+      </Box>
+    </Box>
   );
 }
 
 export default App;
+
+//let pokemonURL = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png`;
+
+// interface Pokemon {
+//   id: number;
+//   name: string,
+//   types: string
+// }
+
+// useEffect(() => {
+//   fetch("https://intern-pokedex.myriadapps.com/api/v1/pokemon")
+//     .then((response) => response.json())
+//     .then((res) => console.log(res))
+//     .catch((err) => setError(err));
+// }, []);
---
+++ src/components/Pokemon.tsx
@@ -0,0 +1,11 @@
+import {useEffect, useState} from "react";
+import App from "../App";
+
+
+const Pokemon = () => {
+    return (
+        <>Todo </>
+    )
+}
+
+export default Pokemon;
